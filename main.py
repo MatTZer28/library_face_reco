6ba7063adb5ec935f5178eed1d9a08701ab3c190
MatTZer28 <78303607+MatTZer28@users.noreply.github.com>
2021-12-19 00:52:13 +0800

    mostly menu ui update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchWindowException
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,10 +16,16 @@ def driver_options():
     options = webdriver.ChromeOptions()
 
     # options.add_experimental_option("detach", True)
-    # 使瀏覽器不會自動關閉
+    # 使瀏覽器不會自動關閉(僅測試用)
 
     options.add_experimental_option("excludeSwitches", ['enable-automation'])
     # 不顯示上方'Chrome正受到自動測試軟體控制'提示
+
+    prefs = {"credentials_enable_service": False, "profile.password_manager_enabled": False}
+    options.add_experimental_option("prefs", prefs)
+    # 關閉Chrome詢問是否儲存密碼選項
+
+    options.add_argument("--use-fake-ui-for-media-stream")
 
     return options
 
@@ -29,32 +35,54 @@ def create_web_driver():
 
     options = driver_options()
 
-    return webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.maximize_window()
 
-
-def login_successful(driver) -> bool:
-    try:
-        wait = WebDriverWait(driver, 600)
-        if wait.until(ec.url_changes(LOGIN_URL)):
-            if driver.current_url == LOGIN_SUCCESSFUL_URL:
-                return True
-            else:
-                return login_session(driver)
-    except TimeoutException:
-        return False
+    return driver
 
 
 def login_session(driver):
-    driver.get(LOGIN_URL)
-    if login_successful(driver):
-        driver.get(MENU_URL)
+    try:
+        wait = WebDriverWait(driver, 1)
+        if wait.until(ec.url_changes(LOGIN_URL)):
+            if driver.current_url == LOGIN_SUCCESSFUL_URL:
+                return
+            else:
+                driver.get(LOGIN_URL)
+                login_session(driver)
+    except TimeoutException:
+        login_session(driver)
+    except NoSuchWindowException:
+        exit()
+
+
+def wait_until_page_loaded(wait, url):
+    try:
+        wait.until(ec.url_to_be(url))
+    except TimeoutException:
+        wait_until_page_loaded(wait, url)
+
+
+def menu_session(driver):
+    try:
+        wait = WebDriverWait(driver, 1)
+        wait_until_page_loaded(wait, MENU_URL)
+        if wait.until(ec.url_changes(MENU_URL)):
+            return
+    except TimeoutException:
+        menu_session(driver)
+    except NoSuchWindowException:
+        exit()
 
 
 def main():
     driver = create_web_driver()
-    driver.maximize_window()
 
+    driver.get(LOGIN_URL)
     login_session(driver)
+
+    driver.get(MENU_URL)
+    menu_session(driver)
 
 
 if __name__ == '__main__':
---
+++ web/camera.js
@@ -0,0 +1,64 @@
+const createVideo = (id, width, height) => {
+    const video = document.createElement("video");
+    video.id = id;
+    video.width = width;
+    video.height = height;
+    video.autoplay = true;
+    video.controls = true;
+    return video;
+};
+
+const createCanvas = (id, width, height) => {
+    const canvas = document.createElement("canvas");
+    canvas.id = id;
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.borderRadius = "10px";
+    canvas.style.borderStyle = "inset"
+    return canvas;
+};
+
+const constraints = {
+    audio: false,
+    video: {
+      facingMode: "user"
+    }
+};
+
+const getCameraStream = video => {
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function success(stream) {
+        video.srcObject = stream;
+      });
+};
+
+const getFrameFromVideo = (video, canvas) => {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+    ctx.translate(video.width, 0);
+    ctx.scale(-1, 1);
+    ctx.drawImage(video, 0, 0, video.width, video.height);
+    ctx.restore();
+    requestAnimationFrame(() => getFrameFromVideo(video, canvas));
+};
+
+const init = () => {
+    const detectSightContainer = document.getElementById("detection_sight_container");
+
+    const width = detectSightContainer.clientWidth - 1 - 50;
+    const height = detectSightContainer.clientHeight - 1 - 50;
+
+    console.log(width, height);
+
+    const video = createVideo("vid", width, height);
+    const canvas = createCanvas("canvas", video.width, video.height);
+
+    getCameraStream(video);
+    getFrameFromVideo(video, canvas);
+
+    detectSightContainer.appendChild(canvas);
+};
+  
+document.getElementById("detection_sight_container").onload = init();
